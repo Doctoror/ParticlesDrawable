b632556df89ab9076148001fe0ca918cde59b138
Yaroslav Mytkalyk <docd1990@gmail.com>
2017-06-01 21:26:15 +0300

    Enabled StrictMode.

--- wallpaperapp/src/main/java/com/doctoror/particleswallpaper/presentation/App.kt
+++ wallpaperapp/src/main/java/com/doctoror/particleswallpaper/presentation/App.kt
@@ -1,9 +1,11 @@
 package com.doctoror.particleswallpaper.presentation
 
 import android.app.Application
+import com.doctoror.particleswallpaper.BuildConfig
 import com.doctoror.particleswallpaper.presentation.di.Injector
 import com.doctoror.particleswallpaper.presentation.di.components.DaggerConfigComponent
 import com.doctoror.particleswallpaper.presentation.di.modules.AppModule
+import android.os.StrictMode
 
 /**
  * Created by Yaroslav Mytkalyk on 01.06.17.
@@ -12,7 +14,22 @@ class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initStrictMode()
         initDagger()
+    }
+
+    private fun initStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build())
+
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build())
+        }
     }
 
     private fun initDagger() {
