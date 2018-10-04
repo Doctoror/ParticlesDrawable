77f6f656a15935c248957d1605b2c713c0e1fa6b
Yaroslav Mytkalyk <docd1990@gmail.com>
2018-10-04 10:01:33 +0300

    Change ConfigurableKeep retention and target

--- library/src/main/java/com/doctoror/particlesdrawable/ConfigurableKeep.java
+++ library/src/main/java/com/doctoror/particlesdrawable/ConfigurableKeep.java
@@ -2,11 +2,20 @@ package com.doctoror.particlesdrawable;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.TYPE;
 
 /**
  * Custom Keep annotation that is controlled by this project proguard config.
  * Some flavors can be built with exposing properties hidden by ConfigurableKeep.
  */
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.CLASS)
+@Target({PACKAGE, TYPE, ANNOTATION_TYPE, CONSTRUCTOR, METHOD, FIELD})
 public @interface ConfigurableKeep {
 }
