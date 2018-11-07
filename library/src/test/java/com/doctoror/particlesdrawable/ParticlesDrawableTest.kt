108e6c591c61c82e3c0ebb192891516b6523c8d3
Yaroslav Mytkalyk <docd1990@gmail.com>
2018-11-07 21:35:01 +0200

    Add new ParticlesDrawableTest

--- library/src/test/java/com/doctoror/particlesdrawable/ParticlesDrawableTest.kt
+++ library/src/test/java/com/doctoror/particlesdrawable/ParticlesDrawableTest.kt
@@ -15,163 +15,284 @@
  */
 package com.doctoror.particlesdrawable
 
-import org.junit.Assert.*
+import android.graphics.Canvas
+import android.graphics.ColorFilter
+import android.graphics.PixelFormat
+import android.os.SystemClock
+import com.doctoror.particlesdrawable.engine.Engine
+import com.doctoror.particlesdrawable.engine.SceneConfigurator
+import com.doctoror.particlesdrawable.model.Scene
+import com.doctoror.particlesdrawable.renderer.CanvasSceneRenderer
+import com.nhaarman.mockito_kotlin.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
+import java.lang.reflect.Modifier
 
 @Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner::class)
 class ParticlesDrawableTest {
 
-    @Test
-    fun testIsRunningByDefault() {
-        assertFalse(ParticlesDrawable().isRunning)
+    private val canvasRenderer: CanvasSceneRenderer = mock()
+    private val scene: Scene = mock()
+    private val sceneConfigurator: SceneConfigurator = mock()
+    private val engine: Engine = mock()
+
+    private val underTest = spy(ParticlesDrawable().apply {
+        overrideMember(this, "canvasRenderer", canvasRenderer)
+        overrideMember(this, "scene", scene)
+        overrideMember(this, "sceneConfigurator", sceneConfigurator)
+        overrideMember(this, "engine", engine)
+    })
+
+    private fun overrideMember(target: Any, name: String, value: Any) {
+        val field = target::class.java.getDeclaredField(name)
+        field.isAccessible = true
+
+        val modifiers = field.javaClass.getDeclaredField("modifiers")
+        modifiers.isAccessible = true
+        modifiers.setInt(field, field.modifiers and Modifier.FINAL.inv())
+
+        field.set(target, value)
     }
 
     @Test
-    fun testIsRunningWhenStarted() {
-        val d = ParticlesDrawable()
-        d.setBounds(0, 0, 10, 10)
-        d.start()
-        try {
-            assertTrue(d.isRunning)
-        } finally {
-            d.stop()
-        }
+    fun setsDimensionsOnBoundsChange() {
+        val width = 240
+        val height = 320
+
+        underTest.setBounds(0, 0, width, height)
+
+        verify(engine).setDimensions(width, height)
     }
 
     @Test
-    fun testIsStopedWhenStopped() {
-        val d = ParticlesDrawable()
-        d.setBounds(0, 0, 10, 10)
-        d.start()
-        d.stop()
-        assertFalse(d.isRunning)
+    fun drawsAndRuns() {
+        val canvas: Canvas = mock()
+
+        underTest.draw(canvas)
+
+        val inorder = inOrder(canvasRenderer, engine)
+        inorder.verify(canvasRenderer).setCanvas(canvas)
+        inorder.verify(engine).draw()
+        inorder.verify(canvasRenderer).setCanvas(null)
+        inorder.verify(engine).run()
     }
 
     @Test
-    fun testSetBoundsWhenRunning() {
-        val d = ParticlesDrawable()
-        d.start()
-        d.setBounds(0, 0, 10, 10)
-        d.stop()
+    fun invalidatesSelfOnRequestRender() {
+        underTest.requestRender()
+        verify(underTest).invalidateSelf()
     }
 
     @Test
-    fun testWithZeroBounds() {
-        val d = ParticlesDrawable()
-        d.setBounds(0, 0, 0, 0)
-        d.start()
-        assertTrue(d.isRunning)
-        d.stop()
-        assertFalse(d.isRunning)
+    fun reqeustsRenderOnScheduleNextFrameWithZeroDelay() {
+        underTest.scheduleNextFrame(0)
+        verify(underTest).requestRender()
     }
 
     @Test
-    fun testMakeBrandNewFrameWithZeroBounds() {
-        val d = ParticlesDrawable()
-        d.makeFreshFrame()
+    fun schedulesSelfOnScheduleNextFrameWithNonZeroDelay() {
+        val delay = 36000L
+
+        underTest.scheduleNextFrame(delay)
+
+        val timeCaptor = argumentCaptor<Long>()
+        verify(underTest).scheduleSelf(any(), timeCaptor.capture())
+
+        assertEquals(SystemClock.uptimeMillis() + delay, timeCaptor.firstValue)
     }
 
     @Test
-    fun testMakeBrandNewFrame() {
-        val d = ParticlesDrawable()
-        d.setBounds(0, 0, 10, 10)
-        d.makeFreshFrame()
+    fun unschedulesSelfOnUnscheduleNextFrame() {
+        underTest.unscheduleNextFrame()
+        verify(underTest).unscheduleSelf(any())
     }
 
     @Test
-    fun testMakeBrandNewFrameWhenRunning() {
-        val d = ParticlesDrawable()
-        d.setBounds(0, 0, 10, 10)
-        d.start()
-        d.makeFreshFrame()
-        d.stop()
+    fun setsAlphaToEngine() {
+        val alpha = 64
+        underTest.alpha = alpha
+        verify(engine).alpha = alpha
     }
 
     @Test
-    @Throws(Throwable::class)
-    fun testParticleRadiusRangeThatRoundsUpToTheSameInt() {
-        val d = ParticlesDrawable()
-        d.setBounds(0, 0, 10, 10)
-        d.setParticleRadiusRange(0.5f, 0.6f)
-        d.makeFreshFrame()
+    fun returnsAlphaFromEngine() {
+        val alpha = 128
+        whenever(engine.alpha).thenReturn(alpha)
+        assertEquals(alpha, underTest.alpha)
     }
 
     @Test
-    fun testSetFrameDelay() {
-        val s = ParticlesDrawable()
-        s.frameDelay = 1
-        assertEquals(1, s.frameDelay)
+    fun setsColorFilterToRenderer() {
+        val colorFilter: ColorFilter = mock()
+        underTest.colorFilter = colorFilter
+        verify(canvasRenderer).setColorFilter(colorFilter)
     }
 
     @Test
-    fun testSetSpeedFactor() {
-        val s = ParticlesDrawable()
-        s.speedFactor = 0f
-        assertEquals(0f, s.speedFactor, ASSERT_DELTA)
+    fun opacityIsTranslucent() {
+        assertEquals(PixelFormat.TRANSLUCENT, underTest.opacity)
     }
 
     @Test
-    fun testSetParticleRadiusRange() {
-        val s = ParticlesDrawable()
-        s.setParticleRadiusRange(0.5f, 0.6f)
-        assertEquals(0.5f, s.particleRadiusMin, ASSERT_DELTA)
-        assertEquals(0.6f, s.particleRadiusMax, ASSERT_DELTA)
+    fun startsEngineOnStart() {
+        underTest.start()
+        verify(engine).start()
     }
 
     @Test
-    fun testSetLineThickness() {
-        val s = ParticlesDrawable()
-        s.lineThickness = 1f
-        assertEquals(1f, s.lineThickness, ASSERT_DELTA)
+    fun stopsEngineOnStop() {
+        underTest.stop()
+        verify(engine).stop()
     }
 
     @Test
-    fun testSetLineLength() {
-        val s = ParticlesDrawable()
-        s.lineLength = 0f
-        assertEquals(0f, s.lineLength, ASSERT_DELTA)
+    fun returnsRunningStateFromEngine() {
+        whenever(engine.isRunning).thenReturn(true)
+        assertTrue(underTest.isRunning)
     }
 
     @Test
-    fun testSetDensity() {
-        val s = ParticlesDrawable()
-        s.density = 1
-        assertEquals(1, s.density)
+    fun forwardsNextFrameToEngine() {
+        underTest.nextFrame()
+        verify(engine).nextFrame()
     }
 
     @Test
-    fun setDensityWhenHasSize() {
-        val s = ParticlesDrawable()
-        s.setBounds(0, 0, 1, 1)
-        s.density = 1
-        assertEquals(1, s.density)
+    fun forwardsMakeFreshFrameToEngine() {
+        underTest.makeFreshFrame()
+        verify(engine).makeFreshFrame()
     }
 
     @Test
-    fun setDensityWhenSizeIsBackTo0() {
-        val s = ParticlesDrawable()
-        s.setBounds(0, 0, 1, 1)
-        s.density = 1
-        s.setBounds(0, 0, 0, 0)
-        s.density = 2
-        assertEquals(2, s.density)
+    fun forwardsMakeFreshFrameWithParticlesOffScreenToEngine() {
+        underTest.makeFreshFrameWithParticlesOffscreen()
+        verify(engine).makeFreshFrameWithParticlesOffscreen()
     }
 
     @Test
-    fun testSetLineColor() {
-        val s = ParticlesDrawable()
-        s.lineColor = 2
-        assertEquals(2, s.lineColor)
+    fun forwardsSetDensityToScene() {
+        val value = 1
+        underTest.density = value
+        verify(scene).density = value
     }
 
     @Test
-    fun testSetParticleColor() {
-        val s = ParticlesDrawable()
-        s.particleColor = 3
-        assertEquals(3, s.particleColor)
+    fun returnsDensityFromScene() {
+        val value = 2
+        whenever(scene.density).thenReturn(value)
+        assertEquals(value, underTest.density)
+    }
+
+    @Test
+    fun forwardsSetFrameDelayToScene() {
+        val value = 3
+        underTest.frameDelay = value
+        verify(scene).frameDelay = value
+    }
+
+    @Test
+    fun returnsFrameDelayFromScene() {
+        val value = 4
+        whenever(scene.frameDelay).thenReturn(value)
+        assertEquals(value, underTest.frameDelay)
+    }
+
+    @Test
+    fun forwardsSetParticleColorToScene() {
+        val value = 5
+        underTest.particleColor = value
+        verify(scene).particleColor = value
+    }
+
+    @Test
+    fun returnsParticleColorFromScene() {
+        val value = 6
+        whenever(scene.particleColor).thenReturn(value)
+        assertEquals(value, underTest.particleColor)
+    }
+
+    @Test
+    fun forwardsSetParticleRadiusRangeToScene() {
+        val minRadius = 7f
+        val maxRadius = 8f
+
+        underTest.setParticleRadiusRange(minRadius, maxRadius)
+
+        verify(scene).setParticleRadiusRange(minRadius, maxRadius)
+    }
+
+    @Test
+    fun returnsParticleRadiusMaxFromScene() {
+        val value = 9f
+        whenever(scene.particleRadiusMax).thenReturn(value)
+        assertEquals(value, underTest.particleRadiusMax)
+    }
+
+    @Test
+    fun returnsParticleRadiusMinFromScene() {
+        val value = 10f
+        whenever(scene.particleRadiusMin).thenReturn(value)
+        assertEquals(value, underTest.particleRadiusMin)
+    }
+
+    @Test
+    fun forwardsSetLineColorToScene() {
+        val value = 11
+        underTest.lineColor = value
+        verify(scene).lineColor = value
+    }
+
+    @Test
+    fun returnsLineColorFromScene() {
+        val value = 12
+        whenever(scene.lineColor).thenReturn(value)
+        assertEquals(value, underTest.lineColor)
+    }
+
+    @Test
+    fun forwardsSetLineLengthToScene() {
+        val value = 13f
+        underTest.lineLength = value
+        verify(scene).lineLength = value
+    }
+
+    @Test
+    fun returnsLineLengthFromScene() {
+        val value = 14f
+        whenever(scene.lineLength).thenReturn(value)
+        assertEquals(value, underTest.lineLength)
+    }
+
+    @Test
+    fun forwardsSetLineThicknessToScene() {
+        val value = 15f
+        underTest.lineThickness = value
+        verify(scene).lineThickness = value
+    }
+
+    @Test
+    fun returnsLineThicknessFromScene() {
+        val value = 16f
+        whenever(scene.lineThickness).thenReturn(value)
+        assertEquals(value, underTest.lineThickness)
+    }
+
+    @Test
+    fun forwardsSetSpeedFactorToScene() {
+        val value = 17f
+        underTest.speedFactor = value
+        verify(scene).speedFactor = value
+    }
+
+    @Test
+    fun returnsSpeedFactorFromScene() {
+        val value = 18f
+        whenever(scene.speedFactor).thenReturn(value)
+        assertEquals(value, underTest.speedFactor)
     }
 }
