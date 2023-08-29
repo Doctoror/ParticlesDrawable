2012ec2fe296dc2e88612396eb9be6d8bb1e85b7
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-08-29 11:17:32 +0200

    Use Kotlin DSL for root build.gradle

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version libs.versions.agp apply false
+    id("com.android.library") version libs.versions.agp apply false
+    id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
+}
