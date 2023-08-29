6227317ff4b9becd6e05e368c613c5ac1c83bc2f
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-08-29 11:16:48 +0200

    Use Kotlin DSL for demo/build.gradle

---
+++ demo/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+
+    namespace = "com.doctoror.particlesdrawable.demo"
+
+    compileSdk = libs.versions.androidCompileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.doctoror.particlesdrawable.demo"
+
+        minSdk = libs.versions.androidMinSdk.get().toInt()
+        targetSdk = libs.versions.androidTargetSdk.get().toInt()
+
+        versionCode = project.properties["VERSION_CODE"].toString().toInt()
+        versionName = project.properties["VERSION_NAME"].toString()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildTypes {
+
+        getByName("debug") {
+            isDebuggable = true
+        }
+
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFile(getDefaultProguardFile("proguard-android.txt"))
+        }
+    }
+
+    // Added only to be able to compile with library variants
+    flavorDimensions += listOf("default")
+
+    productFlavors {
+        create("exposed") {
+        }
+
+        create("production") {
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":library"))
+}
